f2d786706c3fd2cdd16f75a69d678689167bec05
BeginnerKR <49934883+BeginnerKR@users.noreply.github.com>
2020-04-24 15:47:22 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import os
+import asyncio
+import datetime
 
 
 prefix = '짱구야 '
@@ -18,8 +20,40 @@ async def on_ready():
     while True:
         await bot.change_presence(status=discord.Status.online, activity=discord.Activity(name=messages[0], type=discord.ActivityType.playing))
         messages.append(messages.pop(0))
-        await asyncio.sleep(7)
+        await asyncio.sleep(10)
 
+        
+@bot.event
+async def on_member_join(member):
+
+    guild = member.guild
+    channel = discord.utils.get(member.guild.text_channels, id = '695588050536759326')
+
+    join = discord.Embed(description = f'{member}님, {guild}에 입장하셨습니다.', colour = discord.Colour.green())
+    join.set_thumbnail(url = member.avatar_url)
+    join.set_author(name = member.name, icon_url = member.avatar_url)
+    join.set_footer(text = member.guild, icon_url = member.guild.icon_url)
+    join.timestamp = datetime.datetime.utcnow()
+
+    await channel.send(embed = join)
+
+    role = discord.utils.get(member.guild.roles, id=int("696510124113526814"))
+    await member.add_roles(role)
+
+@bot.event
+async def on_member_remove(member):
+
+    guild = member.guild
+    channel = discord.utils.get(member.guild.text_channels, id = '695588050536759326')
+
+    remove = discord.Embed(description = f'{member}님, {guild}에서 퇴장하셨습니다.', colour = discord.Colour.red())
+    remove.set_thumbnail(url = member.avatar_url)
+    remove.set_author(name = member.name, icon_url = member.avatar_url)
+    remove.set_footer(text = member.guild, icon_url = member.guild.icon_url)
+    remove.timestamp = datetime.datetime.utcnow()
+
+    await channel.send(embed = remove)        
+        
 
 @bot.command()
 async def load(ctx, extension):
