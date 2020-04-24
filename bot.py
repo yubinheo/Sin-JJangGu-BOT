e6bd6f994fbab9b147d4067ebf8dae216d509ea5
BeginnerKR <49934883+BeginnerKR@users.noreply.github.com>
2020-04-24 17:59:47 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -55,25 +55,5 @@ async def on_member_remove(member):
     await channel.send(embed = remove)        
         
 
-@bot.command()
-async def load(ctx, extension):
-    bot.load_extension(f'cogs.{extension}')
-
-
-@bot.command()
-async def unload(ctx, extension):
-    bot.unload_extension(f'cogs.{extension}')
-
-
-@bot.command()
-async def reload(ctx, extension):
-    bot.unload_extension(f'cogs.{extension}')
-    bot.load_extension(f'cogs.{extension}')
-
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[ :-3 ]}')
-
-
 access_token = os.environ["BOT_TOKEN"]
 bot.run(access_token)
