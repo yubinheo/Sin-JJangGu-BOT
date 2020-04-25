177e6c3d50b8240065e559bc798dc37e4adefb44
BeginnerKR <49934883+BeginnerKR@users.noreply.github.com>
2020-04-25 16:25:14 +0900

    Update Welcome.py

--- cogs/Welcome.py
+++ cogs/Welcome.py
@@ -15,8 +15,10 @@ class Welcome(commands.Cog):
         guild = member.guild
         channel = discord.utils.get(member.guild.channels, id=int("703107735100850277"))
       
-        await channel.send(f'> {member}님 {guild}에 오신것을 진심으로 환영합니다.\n> <#685107380043907097> 한번 확인해 주시고 활동해 주세요.')
+        em = discord.Embed(description = f'{member}님 {guild}에 오신것을 진심으로 환영합니다. :wave: :tada:\n<#685107380043907097> 한번 확인해 주시고 활동해 주세요.',colour = discord.Colour.blue())
+        em.timestamp = datetime.datetime.utcnow()
 
+        await channel.send(embed = em)
         role = discord.utils.get(member.guild.roles, id=int("684940280667045897"))
         await member.add_roles(role)
 
@@ -27,7 +29,10 @@ class Welcome(commands.Cog):
         guild = member.guild
         channel = discord.utils.get(member.guild.channels, id=int("703107735100850277"))
 
-        await channel.send(f'{member}님이 {guild}에서 나가셨습니다.')
+        em = discord.Embed(description = f'{member}님 {guild}에서 나가셨습니다.', colour = discord.Colour.red())
+        em.timestamp = datetime.datetime.utcnow()
+
+        await channel.send(embed = em)
 
 def setup(bot):
     bot.add_cog(Welcome(bot))
