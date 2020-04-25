7a893757020834984cafd569f7ed4853584454cd
BeginnerKR <49934883+BeginnerKR@users.noreply.github.com>
2020-04-25 14:31:54 +0900

    Update Welcome.py

--- cogs/Welcome.py
+++ cogs/Welcome.py
@@ -14,14 +14,8 @@ class Welcome(commands.Cog):
 
         guild = member.guild
         channel = discord.utils.get(member.guild.channels, id=int("703107735100850277"))
-
-        join = discord.Embed(description=f'{member}님, {guild}에 입장하셨습니다.', colour=discord.Colour.green())
-        join.set_thumbnail(url=member.avatar_url)
-        join.set_author(name=member.name, icon_url=member.avatar_url)
-        join.set_footer(text=member.guild, icon_url=member.guild.icon_url)
-        join.timestamp = datetime.datetime.utcnow()
-
-        await channel.send(embed=join)
+      
+        await channel.send(f'> {member}님 {guild}에 오신것을 진심으로 환영합니다.\n> <#685107380043907097> 한번 확인해 주시고 활동해 주세요.')
 
         role = discord.utils.get(member.guild.roles, id=int("684940280667045897"))
         await member.add_roles(role)
@@ -33,14 +27,7 @@ class Welcome(commands.Cog):
         guild = member.guild
         channel = discord.utils.get(member.guild.channels, id=int("703107735100850277"))
 
-        remove = discord.Embed(
-        description=f'{member}님, {guild}에서 퇴장하셨습니다.', colour=discord.Colour.red())
-        remove.set_thumbnail(url=member.avatar_url)
-        remove.set_author(name=member.name, icon_url=member.avatar_url)
-        remove.set_footer(text=member.guild, icon_url=member.guild.icon_url)
-        remove.timestamp = datetime.datetime.utcnow()
-
-        await channel.send(embed=remove)
+        await channel.send(f'{member}님이 {guild}에서 나가셨습니다.')
 
 def setup(bot):
     bot.add_cog(Welcome(bot))
