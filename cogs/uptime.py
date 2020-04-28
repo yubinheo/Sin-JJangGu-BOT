71d0cc70ec898a0673ec5d93b7575424d3eb4e3c
BeginnerKR <49934883+BeginnerKR@users.noreply.github.com>
2020-04-28 21:06:58 +0900

    Update uptime.py

--- cogs/uptime.py
+++ cogs/uptime.py
@@ -13,15 +13,15 @@ def __init__(self, bot):
 
     @commands.command(aliases = ['업타임'])
     async def uptime(self, ctx):
-		curr_time = time.time()
-		diff = int(round(curr_time - start_time))
-		text = str(datetime.timedelta(seconds=diff))
-		embed = discord.Embed(color=discord.Color.blue())
-		embed.add_field(name="짱구봇 온라인 시간 :-", value=text)
-		try:
-			await ctx.send(embed=embed)
-		except discord.HTTPException:
-			await ctx.send("Uptime is " + text)
+        curr_time = time.time()
+	diff = int(round(curr_time - start_time))
+	text = str(datetime.timedelta(seconds=diff))
+	embed = discord.Embed(color=discord.Color.blue())
+	embed.add_field(name="짱구봇 온라인 시간 :-", value=text)
+	try:
+		await ctx.send(embed=embed)
+	except discord.HTTPException:
+		await ctx.send("Uptime is " + text)
 
 def setup(bot):
 	bot.add_cog(Uptime(bot))
