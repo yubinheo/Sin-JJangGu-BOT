9b9c54a3016a2263add21ab81a444afd3360c1fc
BeginnerKR <49934883+BeginnerKR@users.noreply.github.com>
2020-04-28 21:30:23 +0900

    Update uptime.py

--- cogs/uptime.py
+++ cogs/uptime.py
@@ -14,14 +14,14 @@ def __init__(self, bot):
     @commands.command(aliases = ['업타임'])
     async def uptime(self, ctx):
         curr_time = time.time()
-	diff = int(round(curr_time - start_time))
-	text = str(datetime.timedelta(seconds=diff))
-	embed = discord.Embed(color=discord.Color.blue())
-	embed.add_field(name=f"{self.bot.user.name} 온라인 시간 :-", value=text)
-	try:
-	    await ctx.send(embed=embed)
-	except discord.HTTPException:
-	    await ctx.send(f"{self.bot.user.name} 온라인 시간 " + text)
+	    diff = int(round(curr_time - start_time))
+	    text = str(datetime.timedelta(seconds=diff))
+	    embed = discord.Embed(color=discord.Color.blue())
+	    embed.add_field(name=f"{self.bot.user.name} 온라인 시간 :-", value=text)
+	    try:
+	        await ctx.send(embed=embed)
+	    except discord.HTTPException:
+	        await ctx.send(f"{self.bot.user.name} 온라인 시간 " + text)
 
 def setup(bot):
 	bot.add_cog(Uptime(bot))
