888e0565019625d6d74a201b42482e81f0bc701c
BeginnerKR <49934883+BeginnerKR@users.noreply.github.com>
2020-04-30 15:39:56 +0900

    Add files via upload

---
+++ cogs/Error.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+
+class Error(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        try:
+            if hasattr(ctx.error, 'on_error'):
+                return
+            else:
+                embed = discord.Embed(
+                    titile=f'{ctx.command} 에서 오류 발생', description=f'{ctx.command.qualified_name}\n{error}', Colour=discord.Colour.red())
+                await ctx.send(embed=embed)
+        except:
+            embed = discord.Embed(titile=f'{ctx.command} 에서 오류 발생',
+                                  description=f'{error}', Colour=discord.Colour.red())
+            await ctx.send(embed=emded)
+
+
+def setup(bot):
+    bot.add_cog(Error(bot))
