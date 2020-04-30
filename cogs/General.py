a72d7ad3c856c7d26d7414eb2178e033601f1cae
BeginnerKR <49934883+BeginnerKR@users.noreply.github.com>
2020-04-30 15:28:01 +0900

    Update General.py

--- cogs/General.py
+++ cogs/General.py
@@ -17,22 +17,18 @@ class Basics(commands.Cog):
 
     @commands.command(aliases = ['초대'])
     async def invite(self, ctx):
-        invite = discord.Embed(title = f'{bot.user.name}을 초대하고 싶은가요?', colour = discord.Colour.blue(), timestamp = ctx.message.created_at)
-
+        invite = discord.Embed(title = f'{self.bot.user.name}을 초대하고 싶은가요?', colour = discord.Colour.blue())
+        invite.timestamp = datetime.datetime.utcnow()
         invite.add_field(name = '관리자 역할로 초대하기', value = '[Text link](https://discordapp.com/api/oauth2/authorize?client_id=696508188584968272&permissions=8&scope=bot)', inline = False)
         invite.add_field(name = '추천 역할로 초대하기', value = '[Text link](https://discordapp.com/oauth2/authorize?client_id=696508188584968272&permissions=1103588416&scope=bot)', inline = False)
 
-        outmsg = await ctx.send(embed = invite)
-        await outmsg.delete(delay = 30)
-        await ctx.message.delete()
-
+        await ctx.send(embed = invite)
+       
     @commands.command(aliases = ['문의'])
     async def question(self, ctx):
-        embed = discord.Embed(title = f'{bot.user.name} 공식 디스코드', url = 'https://discord.gg/dn9MbUp',description ='여기로 가면 문의를 할 수 있습니다!', colour = discord.Colour.blue(), timestamp = ctx.message.created_at)
-
-        outmsg = await ctx.send(embed = embed)
-        await outmsg.delete(delay = 30)
-        await ctx.message.delete()
+        embed = discord.Embed(title = f'{self.bot.user.name} 공식 디스코드', url = 'https://discord.gg/dn9MbUp',description ='여기로 가면 문의를 할 수 있습니다!', colour = discord.Colour.blue())
+        embed.timestamp = datetime.datetime.utcnow()
+        await ctx.send(embed = embed)
 
 def setup(bot):
     bot.add_cog(Basics(bot))
